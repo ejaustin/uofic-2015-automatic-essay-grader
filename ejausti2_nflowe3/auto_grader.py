55b590d930439bc6d59be47c275e2103bdfc5619
Emily J <ejanerose@gmail.com>
2015-05-05 01:58:20 -0500

    competition submission.

--- ejausti2_nflowe3/auto_grader.py
+++ ejausti2_nflowe3/auto_grader.py
@@ -8,8 +8,7 @@ from nltk.corpus import wordnet
 from nltk.tokenize import sent_tokenize
 
 
-# ToDo: Record results for each data in the dataset.
-# ToDO: Detemine struct for result storage.
+# Todo: Modify verb tense agreenment to use parse tree associations.
 
 
 # Global vars.
@@ -32,12 +31,58 @@ def debug(out):
 	return
 
 
+# Param: score, scales (a list of 4 thresholds for ranks 5 to 2).
+# Return: number (scaled rank of 5, 4, 3, 2, or 1)
+def scale_scores(score, s5, s4, s3, s2):
+	if score >= s5:
+		return 5
+	elif score >= s4:
+		return 4
+	elif score >= s3:
+		return 3
+	elif score >= s2:
+		return 2
+	else:
+		return 1
+
+
+# Special score scale for score_3a number of sentences and length.
+# Param: score
+# Return: number (scaled rank of 5, 4, 3, 2, or 1)
+def scale_score3a(score):
+	if score > 40:
+		return 1
+	elif score > 30:
+		return 3
+	elif score > 24:
+		return 5
+	elif score > 20:
+		return 4
+	elif score > 18:
+		return 3
+	elif score > 16:
+		return 2
+	else:
+		return 1
+
+
+# Special score scale for the monster score.
+# Param: score
+# Return: number (scaled rank of 5, 4, 3, 2, or 1)
+def scale_monster(score):
+	if score >=19:
+		return 'high'
+	elif score >= 14:
+		return 'medium'
+	else:
+		return 'low'
+
 # Param: word
 # Return: True if the word is illegal, false otherwise.
 def double_check(word):
 	# Manual spelling mistakes double check.
-	manual_set = set(['hyper','global-warming','to','them','his','cannot','they',\
-		'during','him','should','this',"'ve",'where','because', 'becauase','their',\
+	manual_set = set(['to','them','his','cannot','they',\
+		'during','him','should','this',"'ve",'where','because','their',\
 		'what',"'",'since','your', 'everything','we','how','although','others','would',\
 		'anything','could','against','you','among','into','everyone','with','everybody',\
 		'from','.',",",'anyone','until',':',"'s",'than','those','these',"n't",'of','my',\
@@ -59,6 +104,8 @@ def spelling_mistakes(tokens):
 			# Increment wrong count if the token is not valid.
 			wrong = wrong+1
 		total = total+1
+	if total == 0:
+		return 0
 	correctness = 1-(wrong/float(total))
 	return correctness
 
@@ -92,6 +139,8 @@ def verb_agreement(tokens):
 	    # we increment the number of errors
 	    elif pos_tags[index] in plural_noun and pos_tags[index + 1] == singular_verb:
 			errors += 1
+	if num_verbs == 0:
+		return 0
 	correctness = 1-(errors/float(num_verbs))
 	return correctness
 
@@ -130,7 +179,10 @@ def verb_tense(tokens):
 			inconsistencies += 1
 			pres = False
 			past = False
-	correctness = 1-(inconsistencies/float(len(sen)))
+	if len(sen) == 0:
+		return 0
+	correctness = (inconsistencies/float(len(sen)))
+	# Correctness is the essays with more variety in tenses.
 	return correctness
 
 
@@ -147,20 +199,24 @@ def n_of_sentences(data):
 	filtered_words = [w for w in tokens_raw if nonPunct.match(w)]
 	num_sents = len(sents)
 	num_words = len(filtered_words)
+	if num_sents == 0:
+		return 0
 	return num_words/float(num_sents)
 
-''' Code execute block. '''
+
+''' Main code block. '''
 ## Get list of tokenized test files.
 ## Expected path: '../input/test/tokenized'
-err = os.chdir('/input/test/tokenized')
+err = os.chdir('input/test/tokenized')
 path = os.getcwd()
 testfileset = os.listdir(path)
-
+results = []
 for testfilename in testfileset:
 	# Open the test file and tokenize the data.
 	testfile = open(testfilename,'r')
 	data = testfile.read();
 	tokens = word_tokenize(data)
+	print "Grading " + str(testfilename) + '.'
 
 	# Check the spelling mistakes.
 	score_1a = spelling_mistakes(tokens)
@@ -172,7 +228,25 @@ for testfilename in testfileset:
 	score_3a = n_of_sentences(data)
 
 	# Final operations.
+	score_1a = scale_scores(score_1a,0.98,0.97,0.96,0.93)
+	score_1b = scale_scores(score_1b,0.94,0.91,0.82,0.68)
+	# Todo: Change scaling for 1c.
+	#score_1c = scale_scores(score_1c,0.9,0.81,0.72,0.6)
+	score_1c = scale_scores(score_1c,0.41,0.28,0.16,0.1)
+	score_3a = scale_score3a(score_3a)
+
+	clean_score_monster = score_1a + score_1b + score_1c + (2 * score_3a)
+	# Todo: Change scaling for score monster.
+	score_monster = scale_monster(clean_score_monster)
+
+	ret = testfilename + '\t' + str(score_1a) + '\t' +\
+		str(score_1b) + '\t' + str(score_1c) + '\t' + str(score_3a) + '\t' + str(score_monster) + '\n'
+	results.append(ret)
+	print "Done grading " + str(testfilename) + '.'
 	testfile.close()
-	#print testfilename + '\t' + str(score_1a) + '\t' +\
-	#	str(score_1b) + '\t' + str(score_1c) + '\t' + str(score_3a)
-	print score_1b
+#
+err = os.chdir('../../../')
+resultfile = open("result.txt",'w')
+for r in results:
+	resultfile.write(r)
+resultfile.close()
